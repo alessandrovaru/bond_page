b8e72c47ac11a9ad18511f28d1b1edb7ad6feacb
alessandrovaru <alessandrovaru@gmail.com>
2021-04-28 13:33:45 -0400

    Handling errors and sending articles to db

--- app/javascript/components/NewArticle.jsx
+++ app/javascript/components/NewArticle.jsx
@@ -1,41 +1,43 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const NewArticle = () => {
   const [newArticle, setNewArticle] = useState([]);
+  const [articleSent, setArticleSent] = useState(null);
   const [errorA, setErrorA] = useState(null);
-  const [loadingA, setLoadingA] = useState(true);
+  const [loadingA, setLoadingA] = useState(null);
 
-  async function sendArticle() {
+  const sendArticle = async () => {
     const token = document.querySelector('meta[name="csrf-token"]').content;
     const { title, text } = newArticle;
+
     const body = {
       title,
       text,
     };
+
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        "X-CSRF-Token": token,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    };
+
     try {
       setLoadingA(true);
-      await fetch(`/api/v1/articles/create`, {
-        method: "POST",
-        headers: {
-          "X-CSRF-Token": token,
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(body),
-      })
-        .then((response) => {
-          if (response.ok) {
-            console.log(response);
-            return response.json();
-          }
-        })
-        .then(console.log(response));
-    } catch (e) {
-      setErrorA(e);
-      console.log(errorA);
+      const response = await fetch(`/api/v1/articles/create`, requestOptions);
+      const data = await response.json();
+      setArticleSent(data);
+    } catch (error) {
+      setErrorA({ error: true, msg: error.message });
     } finally {
       setLoadingA(false);
     }
-  }
+
+    console.log(requestOptions);
+  };
 
   const handleChange = (e) => {
     setNewArticle({
@@ -63,6 +65,13 @@ const NewArticle = () => {
         </label>
         <input type="submit" value="Submit" />
       </form>
+      {loadingA && <p>Cargando</p>}
+      {articleSent && (
+        <p>
+          Enviado, <Link to={"/article/" + articleSent.id}> read it</Link> dsdss
+        </p>
+      )}
+      {errorA && <p>Error</p>}
     </>
   );
 };
