8834a2c55ac1d3b554c8e20737d9cf44d47d6dcd
alessandrovaru <alessandrovaru@gmail.com>
2021-04-28 21:04:22 -0400

    deleting articles

--- app/javascript/components/Article.jsx
+++ app/javascript/components/Article.jsx
@@ -7,10 +7,12 @@ const Article = () => {
   console.log(params);
 
   const [article, setArticle] = useState([]);
+  const [articleDeleted, setArticleDeleted] = useState(null);
   const [errorA, setErrorA] = useState(null);
   const [loadingA, setLoadingA] = useState(true);
 
   async function getArticle() {
+    setArticleDeleted(false);
     try {
       setLoadingA(true);
       await fetch(`/api/v1/show/${params.id}`)
@@ -27,6 +29,35 @@ const Article = () => {
     }
   }
 
+  const deleteArticle = async () => {
+    const token = document.querySelector('meta[name="csrf-token"]').content;
+
+    const requestOptions = {
+      method: "DELETE",
+      headers: {
+        "X-CSRF-Token": token,
+        "Content-Type": "application/json",
+      },
+    };
+
+    try {
+      setLoadingA(true);
+      const response = await fetch(
+        `/api/v1/destroy/${params.id}`,
+        requestOptions
+      );
+      const data = await response.json();
+      console.log(data);
+      setArticleDeleted(true);
+    } catch (error) {
+      setErrorA({ error: true, msg: error.message });
+    } finally {
+      setLoadingA(false);
+    }
+
+    console.log(requestOptions);
+  };
+
   useEffect(() => {
     getArticle();
   }, []);
@@ -34,6 +65,8 @@ const Article = () => {
     <div>
       <h2>{article.title}</h2>
       <p>{article.text}</p>
+      <button onClick={deleteArticle}>delete</button>
+      {articleDeleted && <p>Borrado</p>}
       <Link to="/articles" className="btn btn-lg custom-button" role="button">
         Back
       </Link>
